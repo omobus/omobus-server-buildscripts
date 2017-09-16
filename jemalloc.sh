1c4e2567c25a5b4fc4766c0ad8457a766a880177
Igor Artemov <i_artemov@omobus.net>
2017-09-16 13:29:21 +0300

    added jemalloc.

---
+++ jemalloc.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# This file is a part of the omobus-server-buildscripts project.
+# Copyright (c) 2006 - 2017 ak-obs, Ltd. <info@omobus.net>.
+# Author: Igor Artemov <i_artemov@ak-obs.ru>.
+
+MYDIR=`pwd`
+SRCDIR=/usr/local/src
+
+cd $SRCDIR
+git clone https://github.com/jemalloc/jemalloc
+cd $SRCDIR/jemalloc
+./autogen.sh
+./configure --silent --disable-cxx
+make
+make install_lib
+make install_include
+make install_bin
+cd $MYDIR
