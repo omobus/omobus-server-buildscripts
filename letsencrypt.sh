d2119bb7b3b445ced563440044b082556d887dc7
Igor Artemov <i_artemov@omobus.net>
2018-06-18 08:27:04 +0300

    fixed letsencrypt cron task.

--- letsencrypt.sh
+++ letsencrypt.sh
@@ -21,6 +21,6 @@ rm /etc/ssl/omobus/lighttpd.pem
 cat $SRCDIR/acme.sh/.data/$1/$1.cer $SRCDIR/acme.sh/.data/$1/$1.key > /etc/ssl/omobus/lighttpd.pem && chmod 600 /etc/ssl/omobus/lighttpd.pem && chown omobus:omobus /etc/ssl/omobus/lighttpd.pem
 cd $MYDIR
 
-echo "33 0 20 * * root $SRCDIR/acme.sh/acme.sh --renew --home $SRCDIR/acme.sh/.data -d $1 -w /var/www/htdocs --use-wget --syslog 3 --log /var/log/letsencrypt.log --log-level 2 && /bin/cat $SRCDIR/acme.sh/.data/$1/$1.cer $SRCDIR/acme.sh/.data/$1/$1.key > /etc/ssl/omobus/lighttpd.pem && /bin/chmod 600 /etc/ssl/omobus/lighttpd.pem && /bin/chown omobus:omobus /etc/ssl/omobus/lighttpd.pem && /bin/systemctl restart lighttpd" > /etc/cron.d/letsencrypt
+echo "33 0 10,20,30 * * root $SRCDIR/acme.sh/acme.sh --renew --home $SRCDIR/acme.sh/.data -d $1 -w /var/www/htdocs --use-wget --syslog 3 --log /var/log/letsencrypt.log --log-level 2 && /bin/cat $SRCDIR/acme.sh/.data/$1/$1.cer $SRCDIR/acme.sh/.data/$1/$1.key > /etc/ssl/omobus/lighttpd.pem && /bin/chmod 600 /etc/ssl/omobus/lighttpd.pem && /bin/chown omobus:omobus /etc/ssl/omobus/lighttpd.pem && /bin/systemctl restart lighttpd" > /etc/cron.d/letsencrypt
 chown root:root /etc/cron.d/letsencrypt
 chmod 644 /etc/cron.d/letsencrypt
