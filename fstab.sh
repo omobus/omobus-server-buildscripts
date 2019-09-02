eb8f5f4ccd9f426c96e00af03a6dc19ac741c167
Igor Artemov <i_artemov@omobus.net>
2019-09-02 21:41:51 +0300

    fstab: fixed ramfs mount.

--- fstab.sh
+++ fstab.sh
@@ -10,7 +10,7 @@ echo 'tmpfs         /var/www/htdocs                     tmpfs  rw,noexec,nosuid,
 echo '/dev/null     /var/www/dev/null                   none   bind                                                        0    0' >> ./fstab.tmp
 echo '/dev/random   /var/lib/omobus-scgi.d/dev/random   none   bind                                                        0    0' >> ./fstab.tmp
 echo '/dev/urandom  /var/lib/omobus-scgi.d/dev/urandom  none   bind                                                        0    0' >> ./fstab.tmp
-echo '#tmpfs        /var/cache/omobus.d/_ramfs          tmpfs  rw,noexec,nosuid,nodev,size=32G,uid=902,gid=905,mode=0700   0    0' >> ./fstab.tmp
+echo '#tmpfs        /var/cache/omobus.d                 tmpfs  rw,noexec,nosuid,nodev,size=32G,uid=902,gid=905,mode=0700   0    0' >> ./fstab.tmp
 
 cp /etc/fstab /etc/fstab-
 cat /etc/fstab- ./fstab.tmp > /etc/fstab
