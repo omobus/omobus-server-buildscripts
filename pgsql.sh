929bac9f935c68de5aa9040aac9e6003b4b02608
Igor Artemov ( <i_artemov@omobus.net>
2022-01-26 18:42:11 +0300

    PostgreSQL: fixed dafault configuration.

--- pgsql.sh
+++ pgsql.sh
@@ -71,9 +71,9 @@ echo "log_directory = 'log'" >> $DBDIR/postgresql.conf
 echo "log_rotation_age = 30d" >> $DBDIR/postgresql.conf
 echo "log_rotation_size = 10MB" >> $DBDIR/postgresql.conf
 echo "" >> $DBDIR/postgresql.conf
-echo "#shared_buffers = 4GB" >> $DBDIR/postgresql.conf
+echo "#shared_buffers = 2GB" >> $DBDIR/postgresql.conf
 echo "#temp_buffers = 512MB" >> $DBDIR/postgresql.conf
-echo "###work_mem = 4GB # changing this parameter leads to performance degradation!!!" >> $DBDIR/postgresql.conf
+echo "#work_mem = 64MB # large value may leads to performance degradation!!!" >> $DBDIR/postgresql.conf
 echo "#maintenance_work_mem = 2GB" >> $DBDIR/postgresql.conf
 echo "#max_locks_per_transaction = 128" >> $DBDIR/postgresql.conf
 chmod 0600 $DBDIR/postgresql.conf && chown postgres:postgres $DBDIR/postgresql.conf
