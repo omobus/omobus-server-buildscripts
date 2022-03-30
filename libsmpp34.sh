8884bfd3a2e452a28d94186305509d9c69652242
Igor Artemov ( <i_artemov@omobus.net>
2022-03-30 13:28:54 +0300

    added libsmpp34.

---
+++ libsmpp34.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# This file is a part of the omobus project.
+# Copyright (c) 2006 - 2022 ak-obs, Ltd. <info@omobus.net>.
+
+FILE=libsmpp34-1.14.0
+MYDIR=`pwd`
+SRCDIR=/usr/local/src
+
+tar -xf ./$FILE.tar.gz -C $SRCDIR
+cd $SRCDIR/$FILE
+libtoolize --force
+aclocal
+autoheader
+automake --force-missing --add-missing
+autoconf
+./configure --silent --includedir=/usr/local/include/libsmpp34
+make install-strip
+cd $MYDIR
